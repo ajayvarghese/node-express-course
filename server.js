beb1675f969dbd9710a3977da9b0053b9b68d3b6
ajayvarghese <ajay.varghese@freighthub.com>
2020-03-22 18:24:34 +0100

    add dynamic GET route

--- server.js
+++ server.js
@@ -5,12 +5,22 @@ const mockUserData=[
 {name:'Mark'},
 {name:'Jill'}
 ]
+
 app.get('/users', function(req,res){
  	res.json({
  	 	success: true,
  	 	message: 'successfully got users. Nice!',
  	 	users: mockUserData
  	})
+})
+
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
 })
 
 app.listen(8000,function(){
